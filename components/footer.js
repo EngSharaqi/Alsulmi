10c18c635fdcd1a293aa61923ac3e45044bf898b
EngSharaqi <muhammadsharaqi@gmail.com>
2022-09-09 18:02:53 +0200

    css enhancements

--- components/footer.js
+++ components/footer.js
@@ -9,7 +9,6 @@ export default function Footer() {
                 <div className={`row ${main.map}`}>
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3623.0946008403116!2d46.74045759999999!3d24.7579454!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2f032378ca29e1%3A0x5aed5c347184e08b!2z2LTYsdmD2Kkg2KfYqNix2KfZh9mK2YUg2KfZhNiz2YTZhdmKINmF2K3Yp9iz2KjZiNmGINmI2YXYsdin2KzYudmI2YYg2YLYp9mG2YjZhtmK2YjZhg!5e0!3m2!1sen!2seg!4v1662674332228!5m2!1sen!2seg"
-                        allowfullscreen=""
                         height="100vh"
                         loading="lazy"
                         referrerpolicy="no-referrer-when-downgrade">
