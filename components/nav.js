763da16b529f6788e94b214cd0afae62bc9e3743
EngSharaqi <muhammadsharaqi@gmail.com>
2022-09-11 17:47:19 +0200

    Edit some

--- components/nav.js
+++ components/nav.js
@@ -1,5 +1,5 @@
 import Links from './links'
-import logo from '../public/images/main_logo.pnh'
+import logo from '../public/images/main_logo.png'
 import Image from 'next/image'
 import Head from 'next/head'
 import main from '../styles/nav.module.css'
